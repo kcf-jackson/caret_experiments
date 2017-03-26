f8d21582d6ddf9ae282558be4df79143ced1bc57
kcf-jackson <kcf.jackson@gmail.com>
2017-03-27 03:12:35 +1100

    Fix data-features combinations

--- data.R
+++ data.R
@@ -11,8 +11,8 @@ hp_generate_response <- function(...) {
 # Simulate datasets and store them in a tibble.
 # Each row corresponds to one dataset.
 caret_tibble <- tibble(
-  n = c(40, 200, 1000, 2000),
-  p = c(3, 30, 100, 200),
+  n = rep(c(40, 200, 1000, 5000), c(4, 4, 4, 4)),
+  p = rep(c(5, 50, 200, 1000), 4),
   beta = pmap(list(n = p, sd = 1 / p), rnorm),
   train_x = map2(n, p, generate_independent_covariates, "numerical"),
   train_y = map2(train_x, beta, hp_generate_response, family = binomial()))
